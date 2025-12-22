c2c5cb801047dad553c6b14c5013c886ba2be225
Heinrich <heinrich9383@gmail.com>
2025-12-22 21:36:10 +0700

    ticket page done, QR otw

---
+++ client/src/pages/user/QRTicket.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { FaArrowLeft, FaClock, FaFilm, FaCalendarAlt, FaDownload } from 'react-icons/fa';
+
+const QRticket = () => {
+    return(
+        <div className="flex justify-center items-center py-10 px-40 w-full">
+            <div className="flex flex-col">
+            <p className="font-bold text-left text-3xl py-5">My Tickets</p>
+            <div className="bg-[#0C1325] flex flex-col w-full p-10 justify-center items-center border-[#334155] border-2 border-solid">
+                <div className="flex flex-wrap gap-10 justify-center">
+
+                </div>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default QRticket;
